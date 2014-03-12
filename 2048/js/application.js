8ae2f96ec76b4d63db59b925f7e957b8ce2de271
gigacardoso <daniel.cardoso18@live.com>
2014-03-12 15:21:49 +0000

    upload 2048 to personal page

---
+++ 2048/js/application.js
@@ -0,0 +1,7 @@
+animationDelay = 50;
+minSearchTime = 50;
+
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+});
